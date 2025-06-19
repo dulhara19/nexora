e36631090b7779a6f071afcac2323c79106b498a
dulhara <wkldulhara@gmail.com>
2025-06-19 16:56:14 +0530

    hybrid response creator: done

--- hybridresponsecreator.py
+++ hybridresponsecreator.py
@@ -1,9 +1,8 @@
-from datetime import datetime
 
-date_time=datetime.now()
+from llmconnector import connector
 
 
-def hybridresponsecreator(structured_question,unstructured_question,structured_asnwers,unstructured_answers):
+def hybridresponsecreator(structured_question,unstructured_question,structured_asnwers,unstructured_answers,date_time):
 
     prompt= f"""
 
@@ -18,26 +17,35 @@ def hybridresponsecreator(structured_question,unstructured_question,structured_a
     - you have current time, so you can use it to provide relevant information based on the current time.
     - use current time to make nice response like "its too late to catch the bus to kadawatha today but be quick, you have another one at the main gate right now"
     - Make nice response comparing current time,date and the context if needed. 
+    - Always wrap your final response inside <final_answer></final_answer> tags.
+    - Try to add supportive informations and be more helpful in your response.
 
     here is the information you have:
     - structured_questions: {structured_question}
     - structured_answers: {structured_asnwers}
     - unstructured_questions: {unstructured_question}
     - unstructured_answers: {unstructured_answers}  
-    - current_time: {date_time} 
+    - current_date_time: {date_time} 
  
     Now, create a friendly and helpful response that includes all the information from the structured and unstructured questions and answers. then wrap it inside <final_answer></final_answer> tags
     
-    example response:
+    Example:
+
+    - structured_questions: ['What is the lunch menu today?', 'when the but to kadawatha arriving at the main gate?']
+    - structured_answers: [Rice and Curry (Rs. 350), Spaghetti (Rs. 400), and Fruit Salad (Rs. 250), 'but to kadawatha 5:00 PM at the main gate']
+    - unstructured_questions: ['who is Ms Yasanthika at NSBM?', 'Who is the head of the computer science department?']
+    - unstructured_answers: [Ms Yasanthika: lecturer in the Computer Science department Cabin-CS building room 101 , Dr. Shafraz : the head of the computer science department. office-located in the CS building room 102]
+    - current_date_time:2025-06-19 16:37:49.664241
+
 
     <final_answer>
     
     Hey there! 😊 i think you missed the first bus to kadawatha but dont worry there is another at 5pm. you have 26mins from now so grab some snacks because it will take 2hours to arrive kadawatha. 
 
-    Also, the lunch menu today is Rice and Curry (Rs. 350), Spaghetti (Rs. 400), and Fruit Salad (Rs. 250). Hope you're hungry! 😋 And you asked about Ms Yasanthika. she is a lecture in the Computer Science department and you can find her in the CS building room 101. 
+    Also, the lunch menu today is Rice and Curry (Rs. 350), Spaghetti (Rs. 400), and Fruit Salad (Rs. 250). Hope you're hungry! 😋 And you asked about Ms Yasanthika. she is a lecture in the Computer Science department and you can find her in the CS building room 101. And head of the computer science department is Dr. Shafraz. If you want to contact him go his office here is the mobile number 
     
     </final_answer>
 
-  
 """
+    return connector(prompt)
     
